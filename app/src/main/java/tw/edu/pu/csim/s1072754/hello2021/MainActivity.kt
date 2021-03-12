680c6435f69e6f4aabbe1ecfef16eef6db652d1b
ying <s1072754@gm.pu.edu.tw>
2021-03-12 13:49:20 +0800

    第一次 Commit

---
+++ app/src/main/java/tw/edu/pu/csim/s1072754/hello2021/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.csim.s1072754.hello2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
