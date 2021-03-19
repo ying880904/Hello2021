d82f875eb23082dd3537aadfd622dc8b44c0ac95
ying <s1072754@gm.pu.edu.tw>
2021-03-19 13:53:05 +0800

    他人加入螢幕觸控方法

--- app/src/main/java/tw/edu/pu/csim/s1072754/hello2021/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1072754/hello2021/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.csim.s1072754.hello2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,17 +12,21 @@ class MainActivity : AppCompatActivity() {
         var txv: TextView = findViewById<TextView>(R.id.txv)
         //txv.text = "Hello ying"
         //TestMaster()
+        //TestDEV()
         txv.setText("嗨，我是陳思穎");
     }
     fun TestMaster(){
         var txv:TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Master分支"
     }
-        //TestDEV()
-    }
     fun TestDEV(){
         var txv:TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Dev分支"
     }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+    var txv:TextView = findViewById<TextView>(R.id.txv)
+    txv.text = "tcyangpu修改螢幕觸控"
+    return true
+}
 }
